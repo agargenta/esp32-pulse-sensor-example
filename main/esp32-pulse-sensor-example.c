f6193587ea9e3efd9388b13f5bc9396f1861f6dc
Aleksandar (Saša) Gargenta <sasa@plusplus.co>
2023-08-07 18:18:15 -0700

    Showcasing how to work with rates

--- main/esp32-pulse-sensor-example.c
+++ main/esp32-pulse-sensor-example.c
@@ -1,12 +1,12 @@
-#include "pulse_sensor.h"
-#include "driver/gpio.h"
-#include "esp_log.h"
+#include <stdio.h>
+#include <assert.h>
 #include <esp_err.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
-#include <stdio.h>
-#include <assert.h>
+#include "esp_log.h"
+#include "driver/gpio.h"
+#include "pulse_sensor.h"
 
 static const char *TAG = "pulse-sensor-example";
 static QueueHandle_t pulse_sensor_notifications_queue;
@@ -42,11 +42,31 @@ static void pulse_sensor_totals_reporter(void *args)
     const pulse_sensor_h pulse_sensor = (pulse_sensor_h)args;
     while (true)
     {
-        ESP_LOGI(TAG, "Pulse sensor totals: pulses=%llu, duration=%llu (usec), cycles=%lu",
+        pulse_sensor_rate_t rate = pulse_sensor_get_total_rate(pulse_sensor);
+        ESP_LOGI(TAG,
+                 "Pulse sensor totals: pulses=%llu, duration=%llu (usec), cycles=%lu, rate=%.3Lf pulses/second",
                  pulse_sensor_get_total_pulses(pulse_sensor),
                  pulse_sensor_get_total_duration(pulse_sensor),
-                 pulse_sensor_get_total_cycles(pulse_sensor));
+                 pulse_sensor_get_total_cycles(pulse_sensor),
+                 rate.duration == 0
+                     ? 0.0L
+                     : rate.pulses / ((long double)rate.duration / 1000000));
         vTaskDelay(5000 / portTICK_PERIOD_MS);
+    }
+}
+
+static void pulse_sensor_rate_reporter(void *args)
+{
+    const pulse_sensor_h pulse_sensor = (pulse_sensor_h)args;
+    while (true)
+    {
+        pulse_sensor_rate_t rate = pulse_sensor_get_current_rate(pulse_sensor);
+        if (rate.pulses > 0)
+        {
+            ESP_LOGI(TAG, "Pulse sensor rate: %.3Lf pulses/second",
+                     rate.pulses / ((long double)rate.duration / 1000000));
+        }
+        vTaskDelay(250 / portTICK_PERIOD_MS);
     }
 }
 
@@ -54,10 +74,10 @@ void app_main(void)
 {
     pulse_sensor_notifications_queue = xQueueCreate(10, sizeof(pulse_sensor_notification_t));
     assert(pulse_sensor_notifications_queue != NULL);
-    BaseType_t r = xTaskCreate(pulse_sensor_notifications_consumer,
-                               "pulse sensor notification consumer task",
-                               2048, NULL, 1, NULL);
-    assert(r == pdPASS);
+
+    assert(xTaskCreate(pulse_sensor_notifications_consumer,
+                       "pulse sensor notification consumer task",
+                       2048, NULL, 1, NULL) == pdPASS);
 
     ESP_ERROR_CHECK(gpio_install_isr_service(0));
     pulse_sensor_config_t pulse_sensor_config = {
@@ -67,8 +87,11 @@ void app_main(void)
     pulse_sensor_h pulse_sensor = NULL;
     ESP_ERROR_CHECK(pulse_sensor_open(pulse_sensor_config, &pulse_sensor));
 
-    r = xTaskCreate(pulse_sensor_totals_reporter,
-                    "pulse sensor totals reporter task",
-                    2048, (void *)pulse_sensor, 1, NULL);
-    assert(r == pdPASS);
+    assert(xTaskCreate(pulse_sensor_rate_reporter,
+                       "pulse sensor rate reporter task",
+                       2048, (void *)pulse_sensor, 1, NULL) == pdPASS);
+
+    assert(xTaskCreate(pulse_sensor_totals_reporter,
+                       "pulse sensor totals reporter task",
+                       2048, (void *)pulse_sensor, 1, NULL) == pdPASS);
 }
